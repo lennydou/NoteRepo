6a0a2a5d644f9fa89c598dfac7373bb04951c40e
douyunliang <douyunliang@xiaomi.com>
2015-03-11 15:33:12 +0800

    first checkin

---
+++ src/main/java/com/lennydou/start/App.java
@@ -0,0 +1,13 @@
+package com.lennydou.start;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
