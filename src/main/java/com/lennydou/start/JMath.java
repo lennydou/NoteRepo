743bc4746d51ab7cd37bdf001fab9b841f6c3041
douyunliang <douyunliang@xiaomi.com>
2015-03-11 15:55:22 +0800

    the second change

---
+++ src/main/java/com/lennydou/start/JMath.java
@@ -0,0 +1,13 @@
+package com.lennydou.start;
+
+/**
+ * Created by dyl on 3/11/15.
+ */
+public class JMath {
+    public int add(int a, int b) {
+        int c = a + b;
+        c = c + 1;
+
+        return c + 2;
+    }
+}
---
+++ src/main/java/com/lennydou/start/Program.java
@@ -0,0 +1,17 @@
+package com.lennydou.start;
+
+/**
+ * Hello world!
+ *
+ */
+public class Program
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        JMath jMath = new JMath();
+        int result = jMath.add(2, 3);
+
+        System.out.println(result);
+    }
+}
